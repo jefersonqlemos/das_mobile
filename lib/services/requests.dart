90870ba02c911fcdd931475ea31bb2be2be777a5
jeferson lemos <jefersonquagliottolemos@hotmail.com>
2023-06-10 21:19:14 -0300

    first commit

--- lib/services/requests.dart
+++ lib/services/requests.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 class Requests {
 
-  static String url = 'https://das-web.shop/api/';
+  static String url = 'http://localhost:8000/api/';
 
   static get(String element){
     return http.get(Uri.parse(url + element));
