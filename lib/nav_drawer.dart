7ce7d02f9aa67e3e21dd827255bc428136fede14
jeferson lemos <jefersonquagliottolemos@hotmail.com>
2023-06-21 18:36:57 -0300

    Revert "s"
    This reverts commit 355576c36a65956efbc56671cd2fe0ae239cbfa4.

--- lib/nav_drawer.dart
+++ lib/nav_drawer.dart
@@ -1,62 +1,57 @@
-import 'package:das_mobile/screens/client_screen.dart';
-import 'package:das_mobile/screens/product_screen.dart';
+import 'dart:convert';
+
+import 'package:das_mobile/carrinho/cart-list.dart';
 import 'package:flutter/material.dart';
 
 class NavDrawer extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          const DrawerHeader(
+          DrawerHeader(
             child: Text(
               'Menu',
               style: TextStyle(color: Colors.white, fontSize: 25),
             ),
             decoration: BoxDecoration(
-              color: Colors.blue,
-              /*image: DecorationImage(
+                color: Colors.blue,
+                /*image: DecorationImage(
                     fit: BoxFit.fill,
                     image: AssetImage('assets/images/cover.jpg')
                 )*/
             ),
           ),
           ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
+            leading: Icon(Icons.home),
+            title: Text('Home'),
             onTap: () => {},
           ),
           ListTile(
-            leading: const Icon(Icons.supervisor_account),
-            title: const Text('Clientes'),
-            onTap: () {
-              Navigator.pop(context); // Close the drawer
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ClientScreen()),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.view_list),
-            title: const Text('Produtos'),
-            onTap: () {
-              Navigator.pop(context); // Close the drawer
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProductScreen()),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.shopping_cart),
-            title: const Text('Carrinhos'),
+            leading: Icon(Icons.supervisor_account),
+            title: Text('Clientes'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
-            leading: const Icon(Icons.inbox),
-            title: const Text('Pedidos'),
+            leading: Icon(Icons.view_list),
+            title: Text('Produtos'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text('Carrinhos'),
+            onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CartList()),
+                )
+            }
+          ),
+          ListTile(
+            leading: Icon(Icons.inbox),
+            title: Text('Pedidos'),
             onTap: () => {Navigator.of(context).pop()},
           ),
         ],
